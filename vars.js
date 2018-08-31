43588bd46dba2fef12efca70f55b20c75693f32b
Levistpatrick <42749570+Levistpatrick@users.noreply.github.com>
2018-08-31 13:57:09 -0500

    Add files via upload

--- vars.js
+++ vars.js
@@ -1,7 +1,8 @@
 let a = 10;
 let b = -25;
 //create two more variables c and d and give them whatever number you want.
-
+let c = 5;
+let d = 3;
 
 const total = a+b+c+d;
 console.log(total);
@@ -12,13 +13,13 @@ console.log(total);
 //new section on string variables
 let convo = "hello ";
 //set name to your name, first and last.
-let name;
+let name;"Ilan";
 //add string convo and name together.
-const greeting =
+const greeting =(Convo+name);
 console.log(greeting);
 
 //make your own message similar to what is above.
-let convo2;
-let
-const
-console.log();
+let convo2="Good afternoon";
+let name2= "Levi";
+const greeting2=(convo2+name2);
+console.log(greeting2);
